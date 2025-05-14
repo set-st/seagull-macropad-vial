8ddd322a925fee44debcb96a3993eba1ca5f255d
Stanislav <stas@pda.in.ua>
2025-05-14 13:41:42 +0300

    first commit

---
+++ config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifdef OLED_ENABLE
+// settings for the oled keyboard demo with Adafruit 0.91" OLED display on the Stemma QT port
+    #define OLED_TIMEOUT 60000
+    #define OLED_FONT_H "lib/glcdfont.c"
+    // #define WPM_ESTIMATED_WORD_SIZE 5
+#endif
